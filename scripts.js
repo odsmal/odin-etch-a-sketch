af32965f8d9ae46c2017129af9612b0fe99f510c
odsmal <44933047+odsmal@users.noreply.github.com>
2024-11-07 22:39:04 +0100

    lowerBrightness

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 let main = document.querySelector("main");
-main.addEventListener("click", addDark);
+main.addEventListener("click", lowerBrightness);
 
-function addDark(event) {
-  console.log(getComputedStyle(event.target).filter);
+function lowerBrightness(event) {
+  // console.log(getComputedStyle(event.target).filter);
+  // console.log(event.target.dataset.brightness == null);
+  // console.log(event.target.getAttribute("style"));
+  // event.target.style.setProperty("filter", `brightness(60%)`);
+  // event.target.dataset.brightness = 60;
+  // console.log(event.target.dataset.brightness);
+
+  if (event.target.dataset.brightness > 0) {
+    const newBrightness = event.target.dataset.brightness - 0.1;
+    event.target.style.setProperty("filter", `brightness(${newBrightness})`);
+    event.target.dataset.brightness = newBrightness;
+    console.log(1);
+  }
 }
+// ${newBrightness}
+//e.target.style.filter.toString()
